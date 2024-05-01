1994d9c40db5f79f2eaace092cee3e1c2ba6e216
rukshani <rukshani@node-3.rr65.rotornet-pg0.clemson.cloudlab.us>
2024-05-01 18:31:35 +0000

    deamon

--- opera-test-tools/iperf_tcp_ns_server.sh
+++ opera-test-tools/iperf_tcp_ns_server.sh
@@ -33,13 +33,13 @@ myArray=("blue" "red" "ns12" "ns13" "ns15" "ns16" "ns17" "ns18" "ns19" "ns20" "n
 # myArray=("cr1" "cr2" "cr3" "cr4" "cr5" "cr6" "cr7" "cr8")
 # myArray=("ns1" "ns2" "ns3" "ns4" "ns5" "ns6" "ns7" "ns8")
 
-cpu_core_id=$(echo "62" | bc)
+cpu_core_id=$(echo "63" | bc)
 
 for i in $(seq 0 $num_namespaces);
 do
-    # cpu_core_id=$(echo "$cpu_core_id+2" | bc)
+    cpu_core_id=$(echo "$cpu_core_id+2" | bc)
     port=$(echo "5100+$i" | bc)
     # numactl -N $nic_local_numa_node ip netns exec ${myArray[$i]} iperf3 -s $server -p $port &
-    sudo taskset --cpu-list $cpu_core_id ip netns exec ${myArray[$i]} iperf3 -s $server -p $port &
+    sudo taskset --cpu-list $cpu_core_id ip netns exec ${myArray[$i]} iperf3 --daemon -s $server -p $port 
 done
 
